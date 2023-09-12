6447889e5d1c1df1618856dcaa22b16078db8977
Rodrigo Castro <castro.rl@gmail.com>
2023-09-12 16:20:21 -0300

    playing with routes and resources

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,9 +9,10 @@ class PostsController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index($id)
     {
         //
+        return "this is the index from PostsController, ID number: " . $id;
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,32 +20,35 @@ Route::get('/', function () {
 });
 
 
-Route::get('/about', function () {
-    // return view('welcome');
-    return "Hi about page";
-});
-
-
-Route::get('/contact', function () {
-    // return view('welcome');
-    return "Hi contact page";
-});
-
-
-Route::get('/post/{id}/{name}', function ($id, $name) {
-    // return view('welcome');
-    return "Hello " . $name . " Your id number " . $id; 
-});
+// Route::get('/post/{id}', [PostsController::class, 'index']);
+Route::resource('/posts', PostsController::class);
 
 
 
 
-Route::group(['middleware' => ['web']], function () {
+
+// Route::get('/about', function () {
+//     // return view('welcome');
+//     return "Hi about page";
+// });
+
+
+// Route::get('/contact', function () {
+//     // return view('welcome');
+//     return "Hi contact page";
+// });
+
+
+// Route::get('/post/{id}/{name}', function ($id, $name) {
+//     // return view('welcome');
+//     return "Hello " . $name . " Your id number " . $id; 
+// });
+
+// Route::group(['middleware' => ['web']], function () {
 
 
     
-});
-
+// });
 
 // Route::get('/dbtest', function () {
 //     return view('dbtest');
